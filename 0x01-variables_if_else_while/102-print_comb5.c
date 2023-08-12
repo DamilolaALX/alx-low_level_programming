74110ab7909d89eb3ce6c8190e0289339768028b
DamilolaALX <Adegunleola@gmail.com>
2023-08-12 14:09:56 -0700

    h

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Description: A program that prints all possible combinations of single-digit
+ *numbers.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	/* print new line */
+	putchar('\n');
+
+	return (0);
+}
