ccd902492a2768538cc1569a561be23d164d4d2e
DamilolaALX <Adegunleola@gmail.com>
2023-08-12 06:44:57 -0700

    Fix description issue for function main in 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point of the program
+ *
+ *Description: Determines if a number is positive, or negative, or is zero.
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
