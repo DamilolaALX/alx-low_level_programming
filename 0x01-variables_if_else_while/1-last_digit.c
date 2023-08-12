0419737fed3f81751a869f4c4324d3f4b9c965f8
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-08-12 15:02:48 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (lgtn == 0)
 	{
-		printf("Last digit of %d is %d\n", n, lgtn);
+		printf("Last digit of %d is %d and is 0\n", n, lgtn);
 	}
 	else if ((lgtn < 6) && (lgtn != 0))
 	{
