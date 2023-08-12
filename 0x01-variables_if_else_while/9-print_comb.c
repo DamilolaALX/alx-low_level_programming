3401959179de6565099e1a248fcf754dd2fcb60e
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-08-12 20:52:49 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,14 @@ int main(void)
 {
 	int i = 48;
 
-	while (i < 58)
+	while (i != 57)
 	{
 		putchar(i);
 		i++;
 		putchar(',');
 		putchar(' ');
 	}
-	/*print new line */
+	/* print new line */
 	putchar('\n');
 
 	return (0);
