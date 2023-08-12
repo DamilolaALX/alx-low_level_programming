311e3b940accc26f5b484416112d4fea165b463c
DamilolaALX <Adegunleola@gmail.com>
2023-08-12 07:13:18 -0700

    Update 2-print_alphabet.c to be betty style compliant

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+/**
+ *main - Entry point of the program.
+ *
+ *Description: Prints the alphabet in lowercase, followed by a new line
+ *
+ *Return: Always 0 (success)
+ */
 
-int main()
+int main(void)
 {
-    char letter = 'a';
+	char letter = 'a';
 /* while the condition is true, print the following */
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
 /* print a new line*/
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
