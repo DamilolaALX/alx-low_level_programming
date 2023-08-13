e9daaf10c73417d6fc6938c3f528355e80d89512
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-08-13 13:15:22 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
-*main - Prints numbers between 00 to 99.
+*main - Entry point of the program.
+*
+*Description: A program that prints all possible combinations of two two-digit numbers.
 *
 *Return: Always 0 (Success)
 */
