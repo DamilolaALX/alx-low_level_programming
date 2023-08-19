8f7c957a1778cd73f511e8a434b968dfa26f3e3d
DamilolaALX <Adegunleola@gmail.com>
2023-08-19 07:18:41 -0700

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,15 +12,18 @@
 
 void print_line(int n)
 {
-	if (n <= '0')
+	int i;
+
+	if (n < 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	while (n > '0')
+	else
 	{
-		_putchar('_');
-		n--;
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
