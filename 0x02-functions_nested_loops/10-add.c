661011746f2c12471b99143f05cd8d2f2df8204e
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-08-20 20:40:39 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- *add- Addds two integers
- *
- *@a, @b: The two integers
- *
- *retun: The sum of two integers
- */
+*add- Adds two integers
+*
+*@a, @b: The two integers
+*
+*retun: The sum of two integers
+*/
 
 int add(int a, int b)
 {
