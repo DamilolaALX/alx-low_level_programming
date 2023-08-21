9d379bed346312b3bd5400d317f72e70caf498c7
DamilolaALX <Adegunleola@gmail.com>
2023-08-21 15:58:22 -0700

    Update main.h and add task 01

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *swap_int- swaps the values of two integers.
+ *@a, @b: integers to be swapped
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
