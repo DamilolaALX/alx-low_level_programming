2aca31e14fe78621d7d719e99ca997e165ac4a82
DamilolaALX <Adegunleola@gmail.com>
2023-09-01 22:52:13 +0000

    Add remaining files and update header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,8 @@
+#ifndef FILE_MAIN                                                                                                                                                       #define FILE_MAIN 
+
 int _putchar(char c);
 int main(int argc, char *argv[] __attribute__ ((unused)));
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
