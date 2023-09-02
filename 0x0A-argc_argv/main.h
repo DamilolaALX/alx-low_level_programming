2425c363c35bd29cec330b88a55c30f6b2237928
DamilolaALX <Adegunleola@gmail.com>
2023-09-02 20:30:59 +0000

    Update main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,4 @@
-#ifndef FILE_MAIN                                                                                                                                                       #define FILE_MAIN 
-
 int _putchar(char c);
-int main(int argc, char *argv[] __attribute__ ((unused)));
+int main(int argc, char *argv[]);
 int _putchar(char c);
 int _atoi(char *s);
-
-#endif
