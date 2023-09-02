6ad61d01d139c9458dfbd99abf2e4451d534bd9b
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-09-02 21:35:22 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,10 @@
 /**
  * main- a program that prints the number of arguments passed into it.
  *
- * @argc: string count
- * @argv: void
+ * @argc: number of arguments
+ * @argv: arrays of arguments
  *
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
@@ -15,5 +15,6 @@ int main(int argc, char *argv[])
 	/* subtract 1 to exclude program name*/
 	printf("%d\n", argc - 1);
 	(void) argv;
+	/*return 0 for success*/
 	return (0);
 }
