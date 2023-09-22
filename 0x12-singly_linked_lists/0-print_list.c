ca2084003fb3233097c8f8a8c124fbd4ef863485
DamilolaALX <Adegunleola@gmail.com>
2023-09-22 22:15:46 +0000

    Add header file, task 00 through 03 and project README file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - A function that prints all the elements of a list
+ *
+ * @h: pointer to list_t
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+	
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
