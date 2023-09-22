40a9b2bdf5d4a32ed8444329e2b05023fb878bf4
DamilolaALX <140358968+DamilolaALX@users.noreply.github.com>
2023-09-22 23:48:44 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,5 +12,6 @@ size_t print_list(const list_t *h);
 int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
